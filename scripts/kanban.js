8d80435eea3e2230b551211f92b088a31db5fac9
Josh <Joshu2020281@live.ytech.edu>
2019-11-07 18:27:09 -0500

    Fixed card from going under the new card button

--- scripts/kanban.js
+++ scripts/kanban.js
@@ -1,118 +1,121 @@
 //  Changes the color when hovered over
 $(function() {
-    $('li').hover(function() {
-        $(this).css("background-color", "#F4F4F4");
-        $(this).css("border-color", "#F4F4F4");
-    }, function() {
-        $(this).css("background-color", "white");
-        $(this).css("border-color", "white");
-    });
+  $('.ui-state-default').hover(function() {
+    $(this).css("background-color", "#F4F4F4");
+    $(this).css("border-color", "#F4F4F4");
+  }, function() {
+    $(this).css("background-color", "white");
+    $(this).css("border-color", "white");
+  });
 });
 
 
 //  Allows the cards to be moved
 function cardSortable() {
-    $(".card, .card").sortable({
-        //  Tolerance: "pointer", makes moving the card more accurate
-        tolerance: "pointer",
-        //  Scroll: false, used so the card doesnt scroll off screen
-        scroll: false,
-        // Adds an animation for placing the card
-        revert: (true, 250),
-        opacity: 0.75,
-        connectWith: ".cardSortable",
-        placeholder: "ui-state-highlight",
 
-        //  Makes the placeholder the same size as the card
+  $(".card, .card").sortable({
+    items: "li:not(.add-new-card, .ui-state-disabled)",
+    //  Tolerance: "pointer", makes moving the card more accurate
+    tolerance: "pointer",
+    //  Scroll: false, used so the card doesnt scroll off screen
+    scroll: false,
+    // Adds an animation for placing the card
+    revert: (true, 250),
+    opacity: 0.75,
+    connectWith: ".cardSortable",
+    placeholder: "ui-state-highlight",
 
-        start: function(e, ui) {
-            /*  The minus 4.30 makes it so that the placeholder doesnt move the
-                other card
-            */
-            ui.placeholder.height(ui.item.height() - 4.30);
-        },
-        cursor: "grabbing",
-        cursorAt: {
-            left: 236 / 2
-        }
-    });
-    $(".card").disableSelection();
+    //  Makes the placeholder the same size as the card
+
+    start: function(e, ui) {
+      /*  The minus 4.30 makes it so that the placeholder doesnt move the
+          other card
+      */
+      ui.placeholder.height(ui.item.height() - 4.30);
+    },
+    cursor: "grabbing",
+    cursorAt: {
+      left: 236 / 2
+    }
+  });
+  $(".card").disableSelection();
 }
 
 
 function editText() {
-    $(function() {
-        $('.cardtext, .portlet-header').dblclick(function(event) {
-            $this = $(this);
-            $this.attr('contenteditable', "true");
-            $this.blur();
-            $this.focus();
-        });
-    })
+  $(function() {
+    $('.cardtext, .portlet-header').dblclick(function(event) {
+      $this = $(this);
+      $this.attr('contenteditable', "true");
+      $this.blur();
+      $this.focus();
+    });
+  })
 }
 
 
 //  Allows the cards to be edited with a double click
 function initPortlets() {
-    //  Allows the card containers to be moved and sorted
-    $(".column").sortable({
-        start: function(e, ui) {
-            /*  The minus 4.30 makes it so that the placeholder doesnt move the
-                other card
-            */
-            ui.placeholder.height(ui.item.height() + 6);
-        },
-        connectWith: ".column",
-        handle: ".portlet-header",
-        cancel: ".portlet-toggle",
-        placeholder: "portlet-placeholder ui-corner-all",
-        cursor: "grabbing"
-    });
+  //  Allows the card containers to be moved and sorted
+  $(".column").sortable({
+    start: function(e, ui) {
+      /*  The minus 4.30 makes it so that the placeholder doesnt move the
+          other card
+      */
+      ui.placeholder.height(ui.item.height() + 6);
+    },
+    connectWith: ".column",
+    handle: ".portlet-header",
+    cancel: ".portlet-toggle",
+    placeholder: "portlet-placeholder ui-corner-all",
+    cursor: "grabbing"
+  });
 
-    $(".portlet:not(.initialized)")
-        .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
-        .find(".portlet-header")
-        .addClass("ui-widget-header ui-corner-all")
-        .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>")
-        .addClass("initialized")
-    $(".cardSortable:not(.initialized)")
-        .append('<button class="add-new-card" onclick="addCard(this)" > + add new card </button>')
-        .addClass("initialized")
+  $(".portlet:not(.initialized)")
+    .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+    .find(".portlet-header")
+    .addClass("ui-widget-header ui-corner-all")
+    .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>")
+    .addClass("initialized")
+  $(".cardSortable:not(.initialized)")
+    .append('<button class="add-new-card" onclick="addCard(this)" > + add new card </button>')
+    .addClass("initialized")
 }
 
 
 function addList() {
-    $(".add-new-list").before('<div class="column"> <div class="portlet"><div class="portlet-header">New list</div><div class="portlet-content"><ul class="cardSortable card"></ul></div></div>');
-    width = $("body").css("width")
+  $(".add-new-list").before('<div class="column"> <div class="portlet"><div class="portlet-header">New list</div><div class="portlet-content"><ul class="cardSortable card"><li class=".ui-state-disabled" style="width: 0px; margin:0; padding:0;"></li></ul></div></div>');
+  width = $("body").css("width")
 
-    //  This increases the width of the page when we add new columns
-    var widthSplit = width.match(/[a-z]+|[^a-z]+/gi);
-    newWidth = parseInt(widthSplit[0]) + 300 + "px"
-    $("body").css("width", newWidth);
-    init()
+  //  This increases the width of the page when we add new columns
+  var widthSplit = width.match(/[a-z]+|[^a-z]+/gi);
+  newWidth = parseInt(widthSplit[0]) + 300 + "px"
+  $("body").css("width", newWidth);
+  init()
 }
 
 function addCard(id) {
-    $(id).before("<li class=\"ui-state-default\"> <p class=\"cardtext\"> New Card </p> <img id =\"del\" src=\"img/x.png\" onclick=\"deleteCard(this)\"/></li>")
+  $(id).before("<li class=\"ui-state-default\"> <p class=\"cardtext\"> New Card </p> <img id =\"del\" src=\"img/x.png\" onclick=\"deleteCard(this)\"/></li>")
 
-    init()
+  init()
 }
-function deleteCard(id){
-    $(id).parent().remove()
+
+function deleteCard(id) {
+  $(id).parent().remove()
 }
 
 function init() {
-    editText()
-    initPortlets()
-    cardSortable()
+  editText()
+  initPortlets()
+  cardSortable()
 }
 
 function changeBG() {
-    var selectedBGColor = document.getElementById("colorpickerbox").value;
-    document.body.style.background = "#" + selectedBGColor;
+  var selectedBGColor = document.getElementById("colorpickerbox").value;
+  document.body.style.background = "#" + selectedBGColor;
 
 }
 
 $(function() {
-    init()
+  init()
 });
